99f16e06af66d82995780f8a1cf3ccf2cc0952f9
fockerev <d1999vin-314@hotmail.com>
2024-02-08 01:10:24 +0900

    Add token limit, token usage ranking

--- cogs/gtp.py
+++ cogs/gtp.py
@@ -14,7 +14,8 @@ class BotCog(commands.Cog):
         self.__messages = [{ "role": "system", "content": self.__gtp_content}]
         self.__history_size = 6
         self.__max_token = 600
-        self.__temperature = 0.7
+        self.__temperature = 1    # 0 ~ 2 default:1
+        self.__token_ranking = {}
 
 
     async def reset_history(self) -> None:
@@ -45,8 +46,35 @@ class BotCog(commands.Cog):
         self.__messages.append({"role": "system","content":txt})
         # print(messages)
 
+    def check_history_size(self):
+        """履歴配列長の確認
 
-    async def ask_chatgpt(self, question:str, reference_message:str = "") -> str:
+        Returns:
+            int : 配列長
+        """
+        return int(len(self.__messages))
+
+    async def delete_old_history(self):
+        """一番古い配列(index = 1) を削除する
+        """
+        while self.__history_size < self.check_history_size() :
+            del self.__messages[1]
+
+
+    async def add_token_ranking(self, author, response):
+        """トークン使用料ランキングを作成する
+
+        Args:
+            author (_type_): メッセージ送信者
+            response (ChatCompletion): OpenAI APIからの応答
+        """
+        if author.id in self.__token_ranking.keys():
+            self.__token_ranking[author.id] += response.usage.completion_tokens
+        else:
+            self.__token_ranking.setdefault(author.id, response.usage.completion_tokens)
+
+
+    async def ask_chatgpt(self, author, question:str, reference_message:str = "") -> str:
         """_summary_
 
         Args:
@@ -72,6 +100,8 @@ class BotCog(commands.Cog):
                                                       max_tokens = self.__max_token,
                                                       temperature = self.__temperature)
 
+            await self.add_token_ranking(author, response)
+
             if len(str(response.choices[0].message.content)) > 0:
                 print(response.usage)
                 return str(response.choices[0].message.content)
@@ -80,18 +110,13 @@ class BotCog(commands.Cog):
         except openai.APIError as e:
             return f"APIでエラーが発生しました: {e}"
 
-    def check_history_size(self):
-        return int(len(self.__messages))
-
-    async def delete_old_history(self):
-        while self.__history_size < self.check_history_size() :
-            del self.__messages[1]
-
+    # 立ち上げ完了時実行
     @commands.Cog.listener()
     async def on_ready(self):
         self.loop_reset.start()
         print("loop start")
 
+    # コマンド実行時
     @commands.hybrid_command(name="ping", brief="ping")
     async def ping(self, ctx):
         await ctx.send("pong")
@@ -113,6 +138,24 @@ class BotCog(commands.Cog):
     async def change(self, ctx, text):
         await self.change_charactor(text)
         await ctx.send("性格を変更しました")
+
+    @commands.hybrid_command(name="ranking", brief="トークン使用量ランキグン")
+    async def ranking(self, ctx):
+        if len(self.__token_ranking) < 1:
+            await ctx.send("まだ誰もAPIを使用していません")
+            return
+
+        self.__token_ranking = sorted(self.__token_ranking.items(), key = lambda x : x[1], reverse= True)
+        embed = discord.Embed(title="Token使用量ランキング", color=discord.Colour.red())
+        for x, dict in enumerate(self.__token_ranking):
+            if 3 < x :
+                break
+
+            user = self.bot.get_user(dict[0])
+            if user is not None:
+                embed.add_field(name=f"{x + 1}位 {user.global_name}", value=f"{dict[1]} token", inline=False)
+
+        await ctx.send(embed=embed)
 
 
     @commands.hybrid_command(name="help", brief="help")
@@ -149,13 +192,13 @@ class BotCog(commands.Cog):
 
         await ctx.send(embed=help_embed)
 
-
+    # ループ処理
     @tasks.loop(minutes=60)
     async def loop_reset(self):
         await self.reset_history()
         print("reset")
 
-
+    # メッセージ受信時実行
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
@@ -177,7 +220,7 @@ class BotCog(commands.Cog):
             print(f"input\t\t{plane_message}")
 
             # GTPへ投げる
-            response = await self.ask_chatgpt(plane_message, reference_message)
+            response = await self.ask_chatgpt(message.author, plane_message, reference_message)
             # チャットに出力
             await message.channel.send(response)
 
