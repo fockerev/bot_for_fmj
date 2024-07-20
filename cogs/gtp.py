1e9d2e0ca368e4ffba6357856732fffe8e212d88
fockerev <d1999vin-314@hotmail.com>
2024-07-21 02:53:04 +0900

    formatter

--- cogs/gtp.py
+++ cogs/gtp.py
@@ -7,23 +7,20 @@ import openai
 import urlextract
 from discord.ext import commands, tasks
 
-OPENAI_API_DICT={
-    "gtp4turbo" :"gpt-4-turbo",
-    "gtp4o": "gpt-4o"
-}
+OPENAI_API_DICT = {"gtp4turbo": "gpt-4-turbo", "gtp4o": "gpt-4o"}
+
 
 class BotCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         self.__model = OPENAI_API_DICT["gtp4o"]
         self.__gtp_content = "Briefly reply unless otherwise mentioned. speaking Kansai dialect"
-        self.__messages = [{ "role": "system", "content": self.__gtp_content}]
-        self.__history_size = 8    # 会話履歴保存数
-        self.__max_token = 800      # 最大Token数
-        self.__temperature = 1      # 0 ~ 2 default:1
+        self.__messages = [{"role": "system", "content": self.__gtp_content}]
+        self.__history_size = 8  # 会話履歴保存数
+        self.__max_token = 800  # 最大Token数
+        self.__temperature = 1  # 0 ~ 2 default:1
         self.__token_ranking = {}
         self.__urlextrator = urlextract.URLExtract()
-
 
     async def reset_history(self) -> None:
         """履歴削除"""
@@ -33,15 +30,12 @@ class BotCog(commands.Cog):
             print("history reset")
         # print(len(messages))
 
-
     async def reset_charactor(self) -> None:
-        """性格をリセットする
-        """
+        """性格をリセットする"""
         del self.__messages[0:]
-        self.__messages.append({"role": "system","content":self.__gtp_content})
+        self.__messages.append({"role": "system", "content": self.__gtp_content})
 
-
-    async def change_charactor(self, txt:str) -> None:
+    async def change_charactor(self, txt: str) -> None:
         """gtpにわたす性格設定を変更する
 
         Args:
@@ -49,7 +43,7 @@ class BotCog(commands.Cog):
         """
 
         del self.__messages[0:]
-        self.__messages.append({"role": "system","content":txt})
+        self.__messages.append({"role": "system", "content": txt})
         # print(messages)
 
     def check_history_size(self):
@@ -61,11 +55,9 @@ class BotCog(commands.Cog):
         return int(len(self.__messages))
 
     async def delete_old_history(self):
-        """一番古い配列(index = 1) を削除する
-        """
-        while self.__history_size < self.check_history_size() :
+        """一番古い配列(index = 1) を削除する"""
+        while self.__history_size < self.check_history_size():
             del self.__messages[1]
-
 
     async def add_token_ranking(self, author, response):
         """トークン使用料ランキングを作成する
@@ -75,7 +67,7 @@ class BotCog(commands.Cog):
             response (ChatCompletion): OpenAI APIからの応答
         """
 
-        if isinstance(self.__token_ranking, dict) is False :
+        if isinstance(self.__token_ranking, dict) is False:
             self.__token_ranking = {}
 
         if author.id in self.__token_ranking.keys():
@@ -83,8 +75,7 @@ class BotCog(commands.Cog):
         else:
             self.__token_ranking.setdefault(author.id, response.usage.total_tokens)
 
-
-    async def ask_chatgpt(self, author, question:str, reference_message:str = "", attachments:str = None) -> str:
+    async def ask_chatgpt(self, author, question: str, reference_message: str = "", attachments: str = None) -> str:
         """_summary_
 
         Args:
@@ -105,7 +96,7 @@ class BotCog(commands.Cog):
         # 画像が添付されている場合それも入力する
         attachments_list = []
         # 対応ファイル形式
-        extention = re.compile(r'.png|.jpg|.gif')
+        extention = re.compile(r".png|.jpg|.gif")
         # 直接メッセージに添付
         if 0 < len(attachments):
             for attach in attachments:
@@ -119,10 +110,9 @@ class BotCog(commands.Cog):
             for url in extracted_urls:
                 if extention.search(url) is not None:
                     attachments_list.append(url)
-                    question = question.replace(url,"")
+                    question = question.replace(url, "")
                 else:
                     return "それ非対応や"
-
 
         print(attachments_list)
 
@@ -133,18 +123,15 @@ class BotCog(commands.Cog):
         if len(attachments_list) > 0:
             image_input = []
             for url in attachments_list:
-                image_input.append({"type": "image_url", "image_url": {"url": url, "detail":"low"}})
+                image_input.append({"type": "image_url", "image_url": {"url": url, "detail": "low"}})
 
-            include_image_content.append({"role": "user", "content":image_input})
+            include_image_content.append({"role": "user", "content": image_input})
 
         pprint.pprint(include_image_content, width=100)
 
         try:
             # ChatGPT APIを呼び出して返答を取得
-            response = openai.chat.completions.create(model = self.__model,
-                                                      messages = include_image_content,
-                                                      max_tokens = self.__max_token,
-                                                      temperature = self.__temperature)
+            response = openai.chat.completions.create(model=self.__model, messages=include_image_content, max_tokens=self.__max_token, temperature=self.__temperature)
 
             await self.add_token_ranking(author, response)
 
@@ -169,18 +156,15 @@ class BotCog(commands.Cog):
     async def ping(self, ctx):
         await ctx.send("pong")
 
-
     @commands.hybrid_command(name="reset_h", brief="会話履歴をリセットする. 60分ごとに自動実行")
     async def reset_h(self, ctx):
         await self.reset_history()
         await ctx.send("会話履歴をリセットしました")
 
-
     @commands.hybrid_command(name="reset_c", brief="性格を初期化する")
     async def reset_c(self, ctx):
         await self.reset_charactor()
         await ctx.send("性格をリセットしました")
-
 
     @commands.hybrid_command(name="chara", brief="引数で入力した文を性格として設定する")
     async def change(self, ctx, text):
@@ -193,10 +177,10 @@ class BotCog(commands.Cog):
             await ctx.send("まだ誰もAPIを使用していません")
             return
 
-        ranking_sorted = sorted(self.__token_ranking.items(), key = lambda x : x[1], reverse= True)
+        ranking_sorted = sorted(self.__token_ranking.items(), key=lambda x: x[1], reverse=True)
         embed = discord.Embed(title="Token使用量ランキング", color=discord.Colour.red())
         for x, dict in enumerate(ranking_sorted):
-            if 3 < x :
+            if 3 < x:
                 break
 
             user = self.bot.get_user(dict[0])
@@ -205,7 +189,6 @@ class BotCog(commands.Cog):
 
         await ctx.send(embed=embed)
 
-
     @commands.hybrid_command(name="help", brief="help")
     async def help(self, ctx, args=None):
         help_embed = discord.Embed()
@@ -213,30 +196,16 @@ class BotCog(commands.Cog):
 
         # If there are no arguments, just list the commands:
         if not args:
-            help_embed.add_field(
-                name="List of supported commands:",
-                value="\n".join([str(i+1)+". "+x.name for i,x in enumerate(self.bot.commands)]),
-                inline=False
-            )
-            help_embed.add_field(
-                name="Details",
-                value="Type `.help <command name>` for more details about each command.",
-                inline=False
-            )
+            help_embed.add_field(name="List of supported commands:", value="\n".join([str(i + 1) + ". " + x.name for i, x in enumerate(self.bot.commands)]), inline=False)
+            help_embed.add_field(name="Details", value="Type `.help <command name>` for more details about each command.", inline=False)
 
         # If the argument is a command, get the help text from that command:
         elif args in command_names_list:
-            help_embed.add_field(
-                name=args,
-                value=self.bot.get_command(args).brief
-            )
+            help_embed.add_field(name=args, value=self.bot.get_command(args).brief)
 
         # If someone is just trolling:
         else:
-            help_embed.add_field(
-                name="Nope.",
-                value="Don't think I got that command, boss!"
-            )
+            help_embed.add_field(name="Nope.", value="Don't think I got that command, boss!")
 
         await ctx.send(embed=help_embed)
 
@@ -263,7 +232,7 @@ class BotCog(commands.Cog):
             print(f"reference\t{reference_message}")
 
             # メンションを除いたテキストを生成
-            plane_message = re.sub(r'<@\d+>', "", message.content)
+            plane_message = re.sub(r"<@\d+>", "", message.content)
             plane_message = plane_message.strip()
             print(f"input\t\t{plane_message}")
 
@@ -275,4 +244,3 @@ class BotCog(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(BotCog(bot))
-
