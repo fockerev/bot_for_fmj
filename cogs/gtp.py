3e15c068dbb5799abe8a6a0f704d50c139351278
fockerev <d1999vin-314@hotmail.com>
2024-07-25 00:31:52 +0900

    history system update

--- cogs/gtp.py
+++ cogs/gtp.py
@@ -1,6 +1,8 @@
 import copy
-import pprint
+import json
 import re
+from logging import config, getLogger
+from pathlib import Path
 
 import discord
 import openai
@@ -12,164 +14,167 @@ OPENAI_API_DICT = {"gtp4turbo": "gpt-4-turbo", "gtp4o": "gpt-4o"}
 
 class BotCog(commands.Cog):
     def __init__(self, bot) -> None:
+        # define logger
+        with open(str((Path(__file__).resolve().parent / ".." / "logging_config.json").resolve()), "r") as f:
+            log_conf = json.load(f)
+        config.dictConfig(log_conf)
+
         self.bot = bot
         self.__model = OPENAI_API_DICT["gtp4o"]
-        self.__gtp_content = "Briefly reply unless otherwise mentioned. speaking Kansai dialect"
-        self.__messages = [{"role": "system", "content": self.__gtp_content}]
-        self.__history_size = 8  # 会話履歴保存数
-        self.__max_token = 800  # 最大Token数
-        self.__temperature = 1  # 0 ~ 2 default:1
-        self.__token_ranking = {}
+        self.__default_charactor: str = "Briefly reply unless otherwise mentioned. speaking Kansai dialect"
+        self.__init_message: list = [{"role": "system", "content": self.__default_charactor}]
+        self.__history: dict = {}
+        self.__history_size: int = 8  # 会話履歴保存数
+        self.__max_token: float = 800  # 最大Token数
+        self.__temperature: float = 1  # 0 ~ 2 default:1
+        self.__token_ranking: dict = {}
         self.__urlextrator = urlextract.URLExtract()
+        self.__logger = getLogger("gtp")
+        self.__save_response = False
 
-    async def reset_history(self) -> None:
+    async def reset_history(self, guild_id: int) -> bool:
         """履歴削除"""
-        # print(len(messages))
-        if len(self.__messages) > 2:
-            del self.__messages[1:]
-            print("history reset")
-        # print(len(messages))
+        if guild_id in self.__history:
+            self.__history[guild_id] = [self.__history[guild_id][0]]
+            self.__logger.info("history reset")
+            return True
+        else:
+            return False
 
-    async def reset_charactor(self) -> None:
+    async def reset_charactor(self, guild_id: int) -> bool:
         """性格をリセットする"""
-        del self.__messages[0:]
-        self.__messages.append({"role": "system", "content": self.__gtp_content})
+        if guild_id in self.__history.keys():
+            self.__history[guild_id] = self.__init_message
+            self.__logger.info("system charactor reset")
+            return True
+        else:
+            return False
 
-    async def change_charactor(self, txt: str) -> None:
+    async def change_charactor(self, guild_id: int, txt: str) -> bool:
         """gtpにわたす性格設定を変更する
 
         Args:
             txt (str): 変更先の性格設定文
         """
+        if guild_id in self.__history.keys():
+            self.__history[guild_id][0] = {"role": "system", "content": txt}
+            self.__logger.info(f"system charactor changed -> {txt}")
+            return True
+        else:
+            return False
 
-        del self.__messages[0:]
-        self.__messages.append({"role": "system", "content": txt})
-        # print(messages)
-
-    def check_history_size(self):
+    def check_history_size(self, guild_id: int) -> int:
         """履歴配列長の確認
 
         Returns:
             int : 配列長
         """
-        return int(len(self.__messages))
+        return int(len(self.__history[guild_id]))
 
-    async def delete_old_history(self):
-        """一番古い配列(index = 1) を削除する"""
-        while self.__history_size < self.check_history_size():
-            del self.__messages[1]
+    async def delete_old_history(self, guild_id: int) -> None:
+        """一番古い履歴(index = 1) を削除する"""
+        while self.__history_size < self.check_history_size(guild_id):
+            del self.__history[guild_id][1]
 
-    async def add_token_ranking(self, author, response):
-        """トークン使用料ランキングを作成する
+    async def parse_message(self, message: discord.message.Message) -> tuple[str, str | None, list]:
+        reference_message = None
+        attachments_list = []
 
-        Args:
-            author (_type_): メッセージ送信者
-            response (ChatCompletion): OpenAI APIからの応答
-        """
-
-        if isinstance(self.__token_ranking, dict) is False:
-            self.__token_ranking = {}
-
-        if author.id in self.__token_ranking.keys():
-            self.__token_ranking[author.id] += response.usage.total_tokens
-        else:
-            self.__token_ranking.setdefault(author.id, response.usage.total_tokens)
-
-    async def ask_chatgpt(self, author, question: str, reference_message: str = "", attachments: str = None) -> str:
-        """_summary_
-
-        Args:
-            author: 発言者
-            question (str): ユーザ側の発言
-            reference_message (str): 同時に入力したい発言
-            attachments : 添付ファイル
-        Returns:
-            str: apiからの応答
-        """
-
-        if reference_message != "":
-            self.__messages.append({"role": "assistant", "content": reference_message})
-
-        await self.delete_old_history()
+        #
+        plane_message = re.sub(r"<@\d+>", "", message.content)
+        plane_message = plane_message.strip()
+        if message.reference is not None:
+            if message.reference.resolved is not None:
+                reference_message = message.reference.resolved.content
 
         # 添付ファイルの抽出
-        # 画像が添付されている場合それも入力する
-        attachments_list = []
         # 対応ファイル形式
         extention = re.compile(r".png|.jpg|.gif")
         # 直接メッセージに添付
-        if 0 < len(attachments):
-            for attach in attachments:
+        if 0 < len(message.attachments):
+            for attach in message.attachments:
                 if extention.search(attach.url) is not None:
                     attachments_list.append(attach.url)
                 else:
-                    return "非対応やで"
-        # URLで添付
-        extracted_urls = self.__urlextrator.find_urls(question)
+                    raise ValueError("そのファイル非対応やで")
+
+        # チャットから画像のURLを抽出
+        extracted_urls = self.__urlextrator.find_urls(plane_message)
         if len(extracted_urls) > 0:
             for url in extracted_urls:
                 if extention.search(url) is not None:
                     attachments_list.append(url)
-                    question = question.replace(url, "")
+                    plane_message = plane_message.replace(url, "")
                 else:
-                    return "それ非対応や"
+                    raise ValueError("そのURL非対応やで")
 
-        print(attachments_list)
+        return plane_message, reference_message, attachments_list
 
-        self.__messages.append({"role": "user", "content": question})
+    async def send_question_gtp(self, question: str, reference: str, attachments: list, guild_id: int) -> tuple[str, int]:
+        self.__logger.info(f"[Question] {question}")
 
-        # 画像は連続で入力すると爆発するので専用のメッセージデータを作成
-        include_image_content = copy.deepcopy(self.__messages)
-        if len(attachments_list) > 0:
-            image_input = []
-            for url in attachments_list:
+        self.__history[guild_id].append({"role": "user", "content": question})
+        if reference is not None:
+            self.__history[guild_id][-1]["content"] += f"\n## 以下へ言及\n{reference}"
+            self.__logger.info(f"[Reference] {reference}")
+
+        input_messages = self.__history[guild_id]
+        image_input = []
+        if len(attachments) > 0:
+            for url in attachments:
                 image_input.append({"type": "image_url", "image_url": {"url": url, "detail": "low"}})
 
-            include_image_content.append({"role": "user", "content": image_input})
+            self.__logger.info(f"[Attachments] {attachments}")
+            image_content = [{"role": "user", "content": image_input}]
+            # Token使用料削減のため画像は履歴として保持しない
+            input_messages += image_content
+        # APIに送る
+        response = openai.chat.completions.create(model=self.__model, messages=input_messages, max_tokens=self.__max_token, temperature=self.__temperature)
+        self.__logger.info(f"[Response] {str(response.choices[0].message.content)}")
 
-        pprint.pprint(include_image_content, width=100)
+        if self.__save_response is True:
+            self.__history[guild_id].append({"role": "assistant", "content": str(response.choices[0].message.content)})
 
-        try:
-            # ChatGPT APIを呼び出して返答を取得
-            response = openai.chat.completions.create(model=self.__model, messages=include_image_content, max_tokens=self.__max_token, temperature=self.__temperature)
+        return str(response.choices[0].message.content), response.usage.total_tokens
 
-            await self.add_token_ranking(author, response)
+    async def token_ranking(self, author: discord.Member, usage: int):
+        if isinstance(self.__token_ranking, dict) is False:
+            self.__token_ranking = {}
 
-            if len(str(response.choices[0].message.content)) > 0:
-                print(response.usage)
-                return str(response.choices[0].message.content)
-            else:
-                return "変な応答帰ってきましたよ"
-        except openai.APIError as e:
-            return f"無理ンゴォオオオオオオオオオオ: {e.message}"
-        except Exception as e:
-            return f"なんかエラー出て無理ンゴォオオオオオオオ {e}"
+        if author.id in self.__token_ranking.keys():
+            self.__token_ranking[author.id] += usage
+        else:
+            self.__token_ranking.setdefault(author.id, usage)
 
     # 立ち上げ完了時実行
     @commands.Cog.listener()
     async def on_ready(self):
         self.loop_reset.start()
-        print("loop start")
-
-    # コマンド実行時
-    @commands.hybrid_command(name="ping", brief="ping")
-    async def ping(self, ctx):
-        await ctx.send("pong")
+        self.__logger.info("loop start")
 
     @commands.hybrid_command(name="reset_h", brief="会話履歴をリセットする. 60分ごとに自動実行")
     async def reset_h(self, ctx):
-        await self.reset_history()
-        await ctx.send("会話履歴をリセットしました")
+        ret = await self.reset_history(guild_id=ctx.guild.id)
+        if ret:
+            await ctx.send("会話履歴をリセットしました")
+        else:
+            await ctx.send("会話履歴のリセットに失敗しました")
 
     @commands.hybrid_command(name="reset_c", brief="性格を初期化する")
     async def reset_c(self, ctx):
-        await self.reset_charactor()
-        await ctx.send("性格をリセットしました")
+        ret = await self.reset_charactor(guild_id=ctx.guild.id)
+        if ret:
+            await ctx.send("性格をリセットしました")
+        else:
+            await ctx.send("性格のリセットに失敗しました")
 
     @commands.hybrid_command(name="chara", brief="引数で入力した文を性格として設定する")
     async def change(self, ctx, text):
-        await self.change_charactor(text)
-        await ctx.send("性格を変更しました")
+        ret = await self.change_charactor(guild_id=ctx.guild.id, txt=text)
+        if ret:
+            await ctx.send("性格を変更しました")
+        else:
+            await ctx.send("性格の変更に失敗しました")
 
     @commands.hybrid_command(name="ranking", brief="トークン使用量ランキグン")
     async def ranking(self, ctx):
@@ -212,34 +217,30 @@ class BotCog(commands.Cog):
     # ループ処理
     @tasks.loop(minutes=60)
     async def loop_reset(self):
-        await self.reset_history()
-        print("reset")
+        if len(self.__history.keys()) > 0:
+            for guild_id in self.__history.keys():
+                await self.reset_history(guild_id)
+        self.__logger.info("cyclic history reset")
 
     # メッセージ受信時実行
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.message.Message):
+        # Bot自身からの入力なら無視
         if message.author == self.bot.user:
             return
 
-        reference_message = ""
         if self.bot.user.id in [member.id for member in message.mentions]:
-            # print("Mention")
-
-            if message.reference is not None:
-                if message.reference.resolved is not None:
-                    reference_message = message.reference.resolved.content
-            print("")
-            print(f"reference\t{reference_message}")
-
-            # メンションを除いたテキストを生成
-            plane_message = re.sub(r"<@\d+>", "", message.content)
-            plane_message = plane_message.strip()
-            print(f"input\t\t{plane_message}")
-
-            # GTPへ投げる
-            response = await self.ask_chatgpt(message.author, plane_message, reference_message, message.attachments)
-            # チャットに出力
-            await message.channel.send(response)
+            try:
+                self.__history.setdefault(message.guild.id, copy.deepcopy(self.__init_message))
+                plane_message, reference_message, attatchments = await self.parse_message(message)
+                response, usage = await self.send_question_gtp(plane_message, reference_message, attatchments, message.guild.id)
+                await self.token_ranking(message.author, usage)
+                await self.delete_old_history(guild_id=message.guild.id)
+                await message.channel.send(response)
+            except Exception as e:
+                self.__logger.exception("error occured in gtp processing")
+                await message.channel.send(f"なんかエラー出た {e}")
+        return
 
 
 async def setup(bot):
